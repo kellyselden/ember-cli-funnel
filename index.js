e306c693e14e382ff2f2108b99cbfc3a0a1e0cd1
Kelly Selden <kellyselden@gmail.com>
2018-01-26 09:31:39 -0800

    Revert "use preprocessTree instead of prototype methods"
    This reverts commit e4b52b510c51248468dc9e253618e81e040494b2.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = {
     this.app = app;
 
     this._initOptions();
+    this._injectFunnel();
   },
   _initOptions() {
     let defaultOptions = {
@@ -24,17 +25,51 @@ module.exports = {
       }
     }
   },
+  _injectFunnel() {
+    if (!this.options.enabled) {
+      return;
+    }
+
+    let options = this.options;
+
+    let appAndDependencies = this.app.appAndDependencies;
+    this.app.appAndDependencies = function() {
+      let tree = appAndDependencies.apply(this, arguments);
+
+      tree = new Funnel(tree, {
+        exclude: options.exclude,
+        description: 'Funnel (appAndDependencies): ' + this.name
+      });
+
+      return tree;
+    };
+
+    let _processedTestsTree = this.app._processedTestsTree;
+    this.app._processedTestsTree = function() {
+      let tree = _processedTestsTree.apply(this, arguments);
+
+      tree = new Funnel(tree, {
+        exclude: options.exclude,
+        description: 'Funnel (_processedTestsTree): ' + this.name
+      });
+
+      return tree;
+    };
+  },
 
   preprocessTree(type, tree) {
-    let options = this.options;
-    if (!options.enabled) {
+    if (!this.options.enabled) {
       return tree;
     }
 
-    tree = new Funnel(tree, {
-      exclude: options.exclude,
-      description: `Funnel (${type}): ${this.name}`
-    });
+    let options = this.options;
+
+    if (type === 'css') {
+      tree = new Funnel(tree, {
+        exclude: options.exclude,
+        description: 'Funnel (css): ' + this.name
+      });
+    }
 
     return tree;
   }
